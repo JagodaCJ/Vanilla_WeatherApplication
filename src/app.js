dcd598397686f5c5b97fbc7f73dd0ed14f58a2a8
JagodaCJ <97749383+JagodaCJ@users.noreply.github.com>
2022-01-26 18:18:55 +0100

    Building HTML forecast through JS

--- src/app.js
+++ src/app.js
@@ -21,6 +21,34 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+     <div class="col-2">
+       <div class="forecast-date">${day}</div>
+       <img
+         src="http://openweathermap.org/img/wn/04d@2x.png"
+         alt=""
+         width="42"
+       />
+       <div class="forecast-temperature">
+         <span class="forecast-temperature-max">18</span>
+         <span class="forecast-temperature-min">12</span>
+       </div>
+     </div>
+   `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -82,3 +110,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+
+displayForecast();
